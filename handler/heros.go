767074521d7aa6cca9d6d9e99519458d320bb0b0
Cheri Hung <cherihung@gmail.com>
2019-11-10 23:58:23 -0500

    get single hero and map to struct

--- handler/heros.go
+++ handler/heros.go
@@ -5,17 +5,18 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 //Hero struct
 type Hero struct {
-	family    string
-	uid       int
-	heroName  string `mapstructure:"hero_name"`
-	firstName string `mapstructure:"first_name"`
-	lastName  string `mapstructure:"last_name"`
+	Family    string
+	UID       int
+	HeroName  string `json:"hero_name"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
 
 //GetHeros handler returning all heros
@@ -25,12 +26,28 @@ func GetHeros(ctx *gin.Context) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var data interface{}
-	_ = json.Unmarshal([]byte(file), &data)
+
+	var data []Hero
+	json.Unmarshal([]byte(file), &data)
 	ctx.JSON(http.StatusOK, data)
 }
 
 //GetHeroByID handler return hero by uid
 func GetHeroByID(ctx *gin.Context) {
+	file, err := ioutil.ReadFile("data/heros.json")
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	var data []Hero
+	json.Unmarshal([]byte(file), &data)
+	var selectedHero Hero
+	for i := range data {
+		if data[i].UID == id {
+			selectedHero = data[i]
+		}
+	}
+
+	ctx.JSON(http.StatusOK, selectedHero)
 }
